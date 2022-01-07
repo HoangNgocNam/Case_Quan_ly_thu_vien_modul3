f879fbd95ceaa7e5cfe5410992aed661aa1d3554
HoangNgocNam <hoangngocnam2310@gmail.com>
2022-01-07 10:46:32 +0700

    create

--- librarian/app/Http/Controllers/UserController.php
+++ librarian/app/Http/Controllers/UserController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    public function create()
+    {
+        $roles = Role::all();
+        return view('users.create',compact('roles'));
+    }
+
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->birthday = $request->birthday;
+        $user->phone = $request->phone;
+        if ($request->hasFile('img')){
+            $img = $request->file('img');
+            $path =$img->store('imgs','public');
+            $user->img = $path;
+        }
+        $user->save();
+        $user->roles()->sync($request->role);
+        return redirect()->route('');
+    }
 }
---
+++ librarian/resources/views/users/create.blade.php
@@ -0,0 +1,15 @@
+<form action="" method="post" enctype="multipart/form-data">
+    @csrf
+    <input type="text" name="name" placeholder="Phập tên">
+    <input type="email" name="email" placeholder="Phập email">
+    <input type="date" name="birthday" placeholder="Phập ngày sinh">
+    <input type="text" name="phone" placeholder="Phập số điện thoại">
+    <input type="file" name="img" placeholder="Thêm ảnh">
+    <hr>
+    @foreach($roles as $role)
+        <input name="role[{{$role->id}}]" class="form-check-input" type="checkbox" value="{{ $role->id }}"
+               id="role-{{$role->name}}">{{$role->name}}
+    @endforeach
+    <br>
+    <button type="submit">Thêm mới</button>
+</form>
