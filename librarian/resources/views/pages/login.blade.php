257593ac29f06ffd171bd4abe6f1a310c7b91bef
mytran123 <mytran091196@gmai.com>
2022-01-09 22:28:06 +0700

    sua mau nut 3 form login-resgister-resetpassword

--- librarian/resources/views/pages/login.blade.php
+++ librarian/resources/views/pages/login.blade.php
@@ -14,7 +14,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6 text-center mb-5">
-                <h1 class="heading-section">Chào mừng bạn đã đến với thư viện sách</h1>
+                <h1 style="color: white">Chào mừng bạn đã đến với thư viện sách</h1>
             </div>
         </div>
         <div class="row justify-content-center">
@@ -43,20 +43,15 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <a href="{{route('auth.showFormChangePassword')}}" class="btn btn-danger" style="color: #fff">Đổi mật khẩu</a>
+                        <div class="form-group" style="text-align: center">
+{{--                            <a href="{{route('auth.showFormChangePassword')}}" class="btn btn-danger" style="color: #fff">Đổi mật khẩu</a>--}}
                             <a href="{{route('auth.showFormRegister')}}" class="btn btn-warning" style="color: #fff">Đăng ký</a>
                             <button type="submit" class="btn btn-success">Đăng Nhập</button>
                         </div>
 
-
-
-
-
-
                     </form>
                     <p class="w-100 text-center">&mdash; Đăng Nhập Với &mdash;</p>
-                    <div class="social d-flex text-center">
+                    <div style="text-align: center">
                         <a href="{{ url('/auth/redirect/google') }}" class="px-2 py-2 mr-md-1 rounded"><i class="fab fa-google-plus-g"></i> Google</a>
                     </div>
                 </div>
