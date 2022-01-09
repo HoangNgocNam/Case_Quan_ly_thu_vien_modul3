2efdd0eea095e516f1691a2be3be9f135f106030
hathu03 <nguyenthuha2411vmh@gmail.com>
2022-01-09 08:46:25 +0700

    pull update

--- librarian/app/Http/Controllers/AuthController.php
+++ librarian/app/Http/Controllers/AuthController.php
@@ -58,7 +58,7 @@ class AuthController extends Controller
     public function logout()
     {
         Auth::logout();
-        toastr()->success('Đăng xuất thành công');
+        toastr()->success('Đăng xuất thành cônge');
         return redirect()->route('auth.login');
     }
 
