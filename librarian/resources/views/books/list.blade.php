800532156c80f612ccbe5c285a8ec43f234f4f6a
mytran123 <mytran091196@gmai.com>
2022-01-10 17:12:56 +0700

    add search button in book-list form

--- librarian/resources/views/books/list.blade.php
+++ librarian/resources/views/books/list.blade.php
@@ -4,14 +4,34 @@
 @section("title","Book")
 @section("title-content","Quản lý sách")
 @section("content")
-    <button class="btn btn-primary mb-3">
-        <a style="color: white; text-decoration: none" href="{{route('books.create')}}">Thêm mới Sách</a>
-    </button>
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-primary mb-3">
+                <a style="color: white; text-decoration: none" href="{{route('books.create')}}">Thêm mới Sách</a>
+            </button>
+        </div>
+        <div class="input-group mb-2 col" style="width: 500px">
+            <input style="height: 42px" type="text" name="keyword" class="form-control" id="searchBook">
+            <div class="input-group-prepend">
+                <div class="input-group-text" style="height: 42px">
+                    <i class="fas fa fa-search"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
     <div class="card mb-4">
     <div class="card-header">
         <i class="fas fa-table me-1"></i>
         Danh sách sách
     </div>
+
+
+
+
+
 {{--    <div class="col-12 col-md-4">--}}
 {{--        <input type="text" name="keyword" class="form-control" id="searchBook">--}}
 {{--    </div>--}}
